74bc478b873dbd4172e6c364f35cc18e20d0de5e
PixelGM <amosprayogo@gmail.com>
2023-06-25 22:29:08 -0700

    using namespace std;

--- Windows Perfomance Checker/main.cpp
+++ Windows Perfomance Checker/main.cpp
@@ -5,24 +5,26 @@
 #include <string>
 #include <regex>
 
-std::string checkPowerPlan() {
+using namespace std;
+
+string checkPowerPlan() {
     // Command to list all power plans
-    std::system("powercfg /list > powercfg.txt");
+    system("powercfg /list > powercfg.txt");
 
     // Open the output file
-    std::ifstream file("powercfg.txt");
+    ifstream file("powercfg.txt");
 
     // Check if Ultimate Performance plan exists and is active
     bool ultimatePerformanceIsActive = false;
-    std::string ultimatePerformanceGUID;
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.find("Ultimate Performance") != std::string::npos) {
+    string ultimatePerformanceGUID;
+    string line;
+    while (getline(file, line)) {
+        if (line.find("Ultimate Performance") != string::npos) {
             // Extract GUID from line
             size_t start = line.find(":") + 2; // Start after "GUID: "
             size_t end = line.find("  ("); // Stop before "  (Ultimate Performance)"
             ultimatePerformanceGUID = line.substr(start, end - start);
-            if (line.find("*") != std::string::npos) {
+            if (line.find("*") != string::npos) {
                 ultimatePerformanceIsActive = true;
             }
             break;
@@ -30,45 +32,46 @@ std::string checkPowerPlan() {
     }
 
     // Remove the output file
-    std::remove("powercfg.txt");
+    remove("powercfg.txt");
 
     if (ultimatePerformanceIsActive) {
-        std::cout << "Power Plan is Already Ultimate Performance" << std::endl;
+        cout << "Power Plan is Already Ultimate Performance" << endl;
     }
     else {
-        std::cout << "Current Power Plan is not Ultimate Performance. Press 2 to optimize it." << std::endl;
+        cout << "Current Power Plan is not Ultimate Performance." << endl;
     }
 
     return ultimatePerformanceGUID;
 }
 
-void setPowerPlan(const std::string& ultimatePerformanceGUID) {
+void setPowerPlan(const string& ultimatePerformanceGUID) {
     // Set Ultimate Performance plan as active
-    std::string command = std::string("powercfg -setactive ") + ultimatePerformanceGUID;
-    if (std::system(command.c_str()) == 0) {
-        std::cout << "Successfully set power plan to Ultimate Performance." << std::endl;
+    string command = string("powercfg -setactive ") + ultimatePerformanceGUID;
+    if (system(command.c_str()) == 0) {
+        cout << "Successfully set power plan to Ultimate Performance." << endl;
     }
     else {
-        std::cout << "Failed to set power plan to Ultimate Performance." << std::endl;
+        cout << "Failed to set power plan to Ultimate Performance." << endl;
     }
 }
 
 void createAndSetPowerPlan() {
     // Create Ultimate Performance plan
-    std::system("powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61");
-    std::cout << "Ultimate Performance plan created." << std::endl;
+    system("powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61");
+    cout << endl;
+    cout << "Ultimate Performance plan created." << endl;
 
     // Recreate the text file
-    std::system("powercfg /list > powercfg.txt");
+    system("powercfg /list > powercfg.txt");
 
     // Open the output file
-    std::ifstream file("powercfg.txt");
+    ifstream file("powercfg.txt");
 
     // Read the new powercfg list and extract GUID of Ultimate Performance plan
-    std::string ultimatePerformanceGUID;
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.find("Ultimate Performance") != std::string::npos) {
+    string ultimatePerformanceGUID;
+    string line;
+    while (getline(file, line)) {
+        if (line.find("Ultimate Performance") != string::npos) {
             // Extract GUID from line
             size_t start = line.find(":") + 2; // Start after "GUID: "
             size_t end = line.find("  ("); // Stop before "  (Ultimate Performance)"
@@ -78,39 +81,39 @@ void createAndSetPowerPlan() {
     }
 
     // Set Ultimate Performance plan as active
-    std::string command = std::string("powercfg -setactive ") + ultimatePerformanceGUID;
-    if (std::system(command.c_str()) == 0) {
-        std::cout << "Successfully set power plan to Ultimate Performance." << std::endl;
+    string command = string("powercfg -setactive ") + ultimatePerformanceGUID;
+    if (system(command.c_str()) == 0) {
+        cout << "Successfully set power plan to Ultimate Performance." << endl;
     }
     else {
-        std::cout << "Failed to set power plan to Ultimate Performance." << std::endl;
+        cout << "Failed to set power plan to Ultimate Performance." << endl;
     }
 
     // Remove the output file
-    std::remove("powercfg.txt");
+    remove("powercfg.txt");
 }
 
 int main() {
     // Print menu option
-    std::cout << "Beginning Performance Check" << std::endl << std::endl;
+    cout << "Beginning Performance Check" << endl << endl;
 
     // Check the power option
-    std::string ultimatePerformanceGUID = checkPowerPlan();
+    string ultimatePerformanceGUID = checkPowerPlan();
     if (!ultimatePerformanceGUID.empty()) {
         // Set the power plan
         setPowerPlan(ultimatePerformanceGUID);
-        std::cout << std::endl;
-        std::cout << "Performance Check Complete" << std::endl;
+        cout << endl;
+        cout << "Performance Check Complete" << endl;
     }
     else {
-        std::cout << "Creating and setting Ultimate Performance plan" << std::endl;
+        cout << "Creating and setting Ultimate Performance plan" << endl;
         // Create and set the power plan
         createAndSetPowerPlan();
-        std::cout << std::endl;
-        std::cout << "Performance Check Complete" << std::endl;
+        cout << endl;
+        cout << "Performance Check Complete" << endl;
     }
 
-    std::cout << "Congrats Your PC has been Optimized! UwU" << std::endl;
+    cout << "Congrats Your PC has been Optimized! UwU" << endl;
 
     return 0;
 }
